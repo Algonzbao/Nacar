78cfbebdec5bd8f2cf0a1886655fb67f2defce4b
Colevo <algonz22@ucm.es>
2025-01-03 14:41:43 +0100

    Images, upscale and shields

--- nacar-web/src/components/Stories.jsx
+++ nacar-web/src/components/Stories.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const StoriesGallery = ({ galleries }) => {
   const [selectedGallery, setSelectedGallery] = useState(null); // Galería seleccionada
   const [currentImageIndex, setCurrentImageIndex] = useState(0); // Índice de la imagen actual
+  const [autoAdvanceInterval, setAutoAdvanceInterval] = useState(null); // Intervalo de avance automático
 
   // Cerrar modal
   const closeModal = () => {
     setSelectedGallery(null);
     setCurrentImageIndex(0);
+    clearInterval(autoAdvanceInterval); // Detener avance automático
   };
 
   // Avanzar a la siguiente imagen
@@ -25,6 +27,18 @@ const StoriesGallery = ({ galleries }) => {
       setCurrentImageIndex(currentImageIndex - 1);
     }
   };
+
+  // Iniciar avance automático
+  useEffect(() => {
+    if (selectedGallery) {
+      const interval = setInterval(() => {
+        nextImage();
+      }, 3000); // Cambia de imagen cada 3 segundos
+      setAutoAdvanceInterval(interval);
+
+      return () => clearInterval(interval); // Limpia el intervalo al desmontar
+    }
+  }, [selectedGallery, currentImageIndex]);
 
   return (
     <div className="flex flex-col items-center w-full">
@@ -48,54 +62,61 @@ const StoriesGallery = ({ galleries }) => {
             {/* Nombre de la galería */}
             <p className="mt-2 text-sm text-white">{gallery.name}</p>
           </div>
-          
         ))}
       </div>
       {/* Modal de galería */}
       {selectedGallery && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50"
-          onClick={closeModal} // Cierra el modal al hacer clic fuera del contenido
-        >
+  <div
+    className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50"
+    onClick={closeModal} // Cierra el modal al hacer clic fuera del contenido
+  >
+  <div className="relative bg-white p-4 rounded-lg max-w-screen-md w-full"
+      onClick={(e) => e.stopPropagation()} // Evita que clics dentro cierren el modal
+    >
+      {/* Indicadores de progreso */}
+      <div className="absolute top-0 left-0 w-full flex gap-1 p-2">
+        {selectedGallery.images.map((_, index) => (
           <div
-            className="relative bg-white p-4 rounded-lg max-w-screen-md w-full"
-            onClick={(e) => e.stopPropagation()} // Evita que clics dentro cierren el modal
-          >
-            {/* Imagen actual */}
-            <div className="relative w-full max-h-[80vh] rounded-md flex">
-              {/* Área izquierda (retroceso) */}
-              <div
-                className="absolute top-0 left-0 h-full w-1/2 cursor-pointer"
-                onClick={prevImage}
-              ></div>
-  
-              {/* Imagen */}
-              <img
-                src={selectedGallery.images[currentImageIndex]}
-                alt={`Imagen ${currentImageIndex + 1}`}
-                className="object-contain w-full max-h-[80vh] rounded-md pointer-events-none"
-              />
-  
-              {/* Área derecha (avanzar) */}
-              <div
-                className="absolute top-0 right-0 h-full w-1/2 cursor-pointer"
-                onClick={nextImage}
-              ></div>
-            </div>
-  
-            {/* Botón de cerrar */}
-            <button
-              className="absolute top-2 right-2 text-red-500 text-2xl font-bold"
-              onClick={closeModal}
-            >
-              &times;
-            </button>
-          </div>
-        </div>
-      )}
+            key={index}
+            className={`h-1 flex-1 bg-gray-300 rounded ${index <= currentImageIndex ? "bg-primary" : ""}`}
+          ></div>
+        ))}
+      </div>
+      <button
+         className="justify-left text-4xl font-extrabold text-arena hover:text-red-700 transition-all transform hover:scale-105"
+        onClick={closeModal}
+      >
+        ×
+      </button>
+
+      {/* Imagen actual */}
+      <div className="relative w-full max-h-[80vh] rounded-md flex">
+        {/* Área izquierda (retroceso) */}
+        <div
+          className="absolute top-0 left-0 h-full w-1/2 cursor-pointer"
+          onClick={prevImage}
+        ></div>
+
+
+        {/* Imagen */}
+        <img
+          src={selectedGallery.images[currentImageIndex].src}
+          alt={`Imagen ${currentImageIndex + 1}`}
+          className="object-contain w-full max-h-[80vh] rounded-md pointer-events-none"
+        />
+
+        {/* Área derecha (avanzar) */}
+        <div
+          className="absolute top-0 right-0 h-full w-1/2 cursor-pointer"
+          onClick={nextImage}
+        ></div>
+      </div>
+    </div>
+  </div>
+)}
+
     </div>
   );
-  
 };
 
 export default StoriesGallery;
--- nacar-web/src/content/galleries/verano2022.md
+++ nacar-web/src/content/galleries/verano2022.md
@@ -3,7 +3,9 @@ year: 2022
 name: Verano 2022
 cover: ../../assets/verano22.png
 images:
-    - https://scontent.fmad19-2.fna.fbcdn.net/v/t39.30808-6/285364612_341590434785098_3995474720735680241_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=3a1ebe&_nc_ohc=svtMEhn6_ikQ7kNvgFcoiNT&_nc_zt=23&_nc_ht=scontent.fmad19-2.fna&_nc_gid=A1AvJVnBprQvHpqsy_YUntl&oh=00_AYB6qsf5QHFweELA-r9dO8HzuxYYRs07WV3ZSZ-WsE_3Pw&oe=676A393A
-    - https://scontent.fmad19-1.fna.fbcdn.net/v/t39.30808-6/287640419_348826257394849_5965318839602127710_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=3a1ebe&_nc_ohc=QzGCzX74cLIQ7kNvgFYTYNR&_nc_zt=23&_nc_ht=scontent.fmad19-1.fna&_nc_gid=AlyJjb8fQeNUZFqpFe_ccnx&oh=00_AYD5g5_LrOdXUs9EMM2_IuMvxmpw6ZRDhts2zgE5zKNTdg&oe=676A2DB2
-    - https://scontent.fmad19-2.fna.fbcdn.net/v/t39.30808-6/292589038_367540075523467_7942608216969502798_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=3a1ebe&_nc_ohc=hUsr43ghqb4Q7kNvgF_VdG4&_nc_zt=23&_nc_ht=scontent.fmad19-2.fna&_nc_gid=AtMxtSp8gPdgaEJ7fBBzaG7&oh=00_AYCfz9T3kR5ZbCpYSxKbsTtoOXxHsTO9p1DOmljzozCj1Q&oe=676A2C7E
+    - ../../assets/storiesveranos/portada22.jpg
+    - ../../assets/storiesveranos/piragua2.jpg
+    - ../../assets/storiesveranos/piragua.jpg
+    - ../../assets/storiesveranos/cubos.jpg
+    - ../../assets/storiesveranos/piscina.jpg
 ---
