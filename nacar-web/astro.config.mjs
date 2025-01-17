760709b1dedd181491b726256fd14389cf5616bc
Colevo <algonz22@ucm.es>
2025-01-17 21:43:07 +0100

    API KEY

--- nacar-web/astro.config.mjs
+++ nacar-web/astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-nocheck
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 
+// Cargar las variables de entorno desde el archivo sendgrid.env
+dotenv.config({ path: './sendgrid.env' });
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  output: 'server',
+  integrations: [tailwind(), react()],
+  adapter: vercel(),
 });
--- nacar-web/src/content/galleries/verano2021.md
+++ nacar-web/src/content/galleries/verano2021.md
@@ -6,7 +6,7 @@ cover: ../../assets/verano21.png
 images:
     - ../../assets/storiesveranos/portada21.jpg
     - ../../assets/storiesveranos/parque.jpg
-    - ../../assets/storiesveranos/piragua3.jpg
-    - ../../assets/storiesveranos/futbol.jpg
+    - ../../assets/storiesveranos/piragua3.png
+    - ../../assets/storiesveranos/prismáticos.jpeg
     - ../../assets/storiesveranos/mural.jpg
 ---
